f5063652f7b4b9272731b3c661b91c71cb3af0f8
KangJiyeon <hihello516@naver.com>
2025-01-29 17:32:59 +0900

    update styled-components

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import GlobalStyle from './assets/styles/global-styles.ts';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './assets/styles/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+        <StrictMode>
+            <GlobalStyle />
+            <App />
+        </StrictMode>
+    </ThemeProvider>
+);
